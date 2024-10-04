e8d301599c113381a7967f6cd518996a981823b0
raghav <raghavt.hestabit@gmail.com>
2024-10-04 16:51:13 +0530

    added pdf rendering using url and path

---
+++ app/src/main/java/com/pdfrender/PdfEngine.kt
@@ -0,0 +1,10 @@
+package com.pdfrender
+
+/**
+ * Created by Raghav on 04,Oct,2024
+ */
+
+enum class PdfEngine(val value: Int) {
+    INTERNAL(100),
+    GOOGLE(200)
+}
---
+++ app/src/main/java/com/pdfrender/PdfQuality.kt
@@ -0,0 +1,11 @@
+package com.pdfrender
+
+/**
+ * Created by Raghav on 04,Oct,2024
+ */
+
+enum class PdfQuality(val ratio: Int) {
+    FAST(1),
+    NORMAL(2),
+    ENHANCED(3)
+}
